132811bc48427716a83c98daa5035413bf01f536
Andrew Moroco <andrew.moroco@gmail.com>
2022-04-24 19:45:04 +0900

    setup basic server and skeleton of routes

---
+++ server/server.js
@@ -0,0 +1,53 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+//Get All Restaurants
+app.get("/api/v1/restaurants", async (req, res) => {
+    res.status(200).json({
+        status: "success",
+        data: {
+            restaurants: ["McDonalds", "Wendys"]
+        },
+    });
+});
+
+//Get A Restaurant
+app.get("/api/v1/restaurants/:id", async (req, res) => {
+    console.log(req.params)
+});
+
+//Create A Restaurant
+app.post("/api/v1/restaurants", async (req, res) => {
+    console.log(req)
+});
+
+//Update A Restaurant
+app.put("/api/v1/restaurants/:id", async (req, res) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    res.status(200).json({
+        status: "success",
+        data: {
+            restaurants: ["McDonalds", "Wendys"]
+        },
+    });
+});
+
+//Delete A Restaurant
+app.delete("/api/v1/restaurants/:id", async (req, res) => {
+    res.status(204).json({
+        status: "success",
+    });
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
