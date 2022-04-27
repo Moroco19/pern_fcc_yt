5999966664c19841e4d2a9d81a1d69197af02ee7
Andrew Moroco <andrew.moroco@gmail.com>
2022-04-27 18:11:28 +0900

    finishing front end skeleton

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import RestaurantDetailPage from "./routes/RestaurantDetailPage";
+import UpdatePage from "./routes/UpdatePage";
 
 const App = () => {
     return (
         <div>
-            App
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/restaurants/:id/update" element={<UpdatePage />} />
+                    <Route path="/restaurants/:id/" element={<RestaurantDetailPage />} />
+                </Routes>
+            </Router>
         </div>
     )
 }
