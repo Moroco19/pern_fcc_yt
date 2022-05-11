1311c067d4c1d9e141dcf6ee830efe471d357245
Andrew Moroco <andrew.moroco@gmail.com>
2022-05-11 23:58:16 +0900

    linked add restaurant component to db and enabled adding of new restaurants via ui

--- client/src/components/AddRestaurant.jsx
+++ client/src/components/AddRestaurant.jsx
@@ -1,18 +1,41 @@
-import React from 'react'
+import React, { useContext, useState } from 'react';
+import RestaurantFinder from '../apis/RestaurantFinder';
+import { RestaurantsContext } from '../context/RestaurantsContext';
 
 const AddRestaurant = () => {
+    const [name, setName] = useState("");
+    const [location, setLocation] = useState("");
+    const [priceRange, setPriceRange] = useState("Price Range");
+    const { addRestaurants } = useContext(RestaurantsContext);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await RestaurantFinder.post("/", {
+                name,
+                location,
+                price_range: priceRange
+            });
+            console.log(response);
+            addRestaurants(response.data.data.restaurants);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
   return (
     <div className="mb-4">
         <form action="">
             <div className="form-row">
                 <div className="col">
-                    <input type="text" className="form-control" placeholder="name" />
+                    <input value={name} onChange={e => setName(e.target.value)} type="text" className="form-control" placeholder="name" />
                 </div>
                 <div className="col">
-                    <input type="text" className="form-control" placeholder="location" />
+                    <input value={location} onChange={e => setLocation(e.target.value)} type="text" className="form-control" placeholder="location" />
                 </div>
                 <div className="col">
-                    <select type="text" className="custom-select my-1 mr-sm-2" placeholder="location">
+                    <select value={priceRange} onChange={e => setPriceRange(e.target.value)} type="text" className="custom-select my-1 mr-sm-2" placeholder="location">
                         <option disabled>Price Range</option>
                         <option value="1">1</option>
                         <option value="2">2</option>
@@ -21,7 +44,7 @@ const AddRestaurant = () => {
                         <option value="5">5</option>
                     </select>
                 </div>
-                <button className="btn btn-primary">Add</button>
+                <button onClick={handleSubmit} type="submit" className="btn btn-primary">Add</button>
             </div>
         </form>
     </div>
