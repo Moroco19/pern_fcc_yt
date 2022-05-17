35af2cb63dfd73202dd2212744619da614d9be25
Andrew Moroco <andrew.moroco@gmail.com>
2022-05-17 18:56:17 +0900

    setup add review component, finished out reviews, and updated server routing

--- client/src/routes/RestaurantDetailPage.jsx
+++ client/src/routes/RestaurantDetailPage.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from 'react'
 import { useParams } from 'react-router-dom';
 import RestaurantFinder from '../apis/RestaurantFinder';
+import AddReview from '../components/AddReview';
 import Reviews from '../components/Reviews';
 import StarRating from '../components/StarRating';
 import { RestaurantsContext } from '../context/RestaurantsContext';
@@ -13,7 +14,8 @@ const RestaurantDetailPage = () => {
     const fetchData = async () => {
       try {
         const response = await RestaurantFinder.get(`/${id}`);
-        setSelectedRestaurant(response.data.data.restaurants);
+        console.log(response.data.data)
+        setSelectedRestaurant(response.data.data);
       } catch(err) {
         console.log(err);
       }
@@ -25,9 +27,11 @@ const RestaurantDetailPage = () => {
     <div>
         {selectedRestaurant && (
           <>
+            <h1 className="text-center display-1">{selectedRestaurant.restaurants.name}</h1>
             <div className="mt-3">
-              <Reviews />
+              <Reviews reviews={selectedRestaurant.reviews} />
             </div>
+            <AddReview />
           </>
         )}
     </div>
